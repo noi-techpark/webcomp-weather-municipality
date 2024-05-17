71c9608a5d44a76119544c0282558c6c8d7cd260
RudiThoeni <r.thoeni@noi.bz.it>
2024-05-17 10:20:34 +0200

    setting right license info

--- src/pointsOfInterest.js
+++ src/pointsOfInterest.js
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
 //
-// SPDX-License-Identifier: MPL-2.0
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
 export function addPointsOfInterestLayer(markers_list) {
     this.pointsOfInterest.map(pointOfInterest => {
